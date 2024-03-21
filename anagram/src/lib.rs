1b212751d5ce4692b4de6efc6daefcea48405c87
suri312006 <suri312006@gmail.com>
2024-03-21 16:09:55 -0700

    finished anagram

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,5 +1,37 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let mut vectorized_word: Vec<char> = String::from(word)
+        .to_lowercase()
+        .chars()
+        .into_iter()
+        .collect();
+    vectorized_word.sort();
+
+    let mut anagrams = HashSet::new();
+
+    for possible_anagram in possible_anagrams {
+        if word.to_lowercase() == *possible_anagram.to_lowercase() {
+            continue;
+        }
+
+        if is_anagram(&vectorized_word, possible_anagram) {
+            anagrams.insert(*possible_anagram);
+        }
+    }
+
+    anagrams
+}
+
+fn is_anagram<'b>(word: &Vec<char>, possible_anagram: &'b str) -> bool {
+    // ideally we would only want to do this once
+    let mut possible_anagram: Vec<char> = String::from(possible_anagram)
+        .to_lowercase()
+        .chars()
+        .into_iter()
+        .collect();
+
+    possible_anagram.sort();
+
+    *word == possible_anagram
 }
